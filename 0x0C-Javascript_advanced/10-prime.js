2f629958ffe977cecb288580081e0b7ed6bef66e
Santiago Yanguas <santitoya-2001@outlook.com>
2020-09-22 19:13:45 -0500

    [Feature] Execution stack & timing execution

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,29 @@
+// Prime numbers & timing execution
+function countPrimeNumbers() {
+  let count = 0;
+  for (let i = 2; i <= 100; i++) {
+    let isprime = false;
+    for (let j = 2; j <= i; j++) {
+      if (i % j == 0) {
+        isprime = false;
+        break;
+      }
+    }
+
+    if (isprime) {
+      ++count;
+    }
+  }
+
+  return count;
+}
+
+let start = performance.now();
+for (let k = 0; k < 100; ++k) {
+  countPrimeNumbers();
+}
+let end = performance.now();
+
+let final = end - start;
+console.log(`Execution time of calculating prime numbers 100 times was ${final} milliseconds.`);
+
