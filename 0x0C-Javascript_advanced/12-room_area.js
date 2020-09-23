459a408ca26a3acbb90cde9fe7ce91e17d7a9d82
Santiago Yanguas <santitoya-2001@outlook.com>
2020-09-22 19:15:12 -0500

    [Feature] Binding + Closure

---
+++ 0x0C-Javascript_advanced/12-room_area.js
@@ -0,0 +1,12 @@
+// Binding
+let roomDimensions = {
+  width: 50,
+  length: 100,
+
+  getArea() {
+    return (this.width * this.length);
+  }
+};
+
+let boundGetArea = roomDimensions.getArea.bind(roomDimensions);
+boundGetArea();
