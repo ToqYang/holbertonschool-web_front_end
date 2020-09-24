3aea786aafd4e222747bb503ded2fec04a0265a7
Santiago Yanguas <santitoya-2001@outlook.com>
2020-09-24 15:11:34 -0500

    [Feature] Chain DOM

---
+++ 0x0D-JQuery_advanced/3-dev.js
@@ -0,0 +1,10 @@
+//  Chain DOM elements
+function createFamilyTree() {
+  $("body").append("<table>\n<thead style=\"font-weight: bold; font-size: 2rem;\">\n<tr>\n<th>Firstname</th>\n<th>Lastname</th>\n</tr>\n</thead>\
+  \n<tbody style=\"font-weight: 500; font-size: 1.5rem;\">\n<tr>\n<td>Guillaume</td>\n<td>Salva</td>\n</tr>\n<tr>\n<td>Paulette</td>\n\
+  <td>Salva</td>\n</tr>\n<tr>\n<td>Antoine</td>\n<td>Salva</td>\n</tr>\n</tbody>\n</table>");
+}
+
+$( document ).ready(function() {
+  createFamilyTree();
+});
